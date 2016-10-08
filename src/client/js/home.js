baafecab51f249bc962e947956adf3581d1e221d
Matteo Leoni <leoni.matteo@gmail.com>
2016-10-08 10:15:49 +0200

    new popup

--- src/client/js/home.js
+++ src/client/js/home.js
@@ -19,4 +19,14 @@ jQuery(document).ready(function($){
 
 	$("#arrival-date, #departure-date").datepicker({});
 
+	$.magnificPopup.open({
+  		items: {
+    src: '#pop'
+  },
+  type: 'inline'
+
+  // You may add options here, they're exactly the same as for $.fn.magnificPopup call
+  // Note that some settings that rely on click event (like disableOn or midClick) will not work here
+}, 0);
+
 });
