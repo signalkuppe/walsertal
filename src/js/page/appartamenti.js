1b3276e5d2ff18af4992ccdcc857c2dfa010bd0d
Matteo Leoni <leoni.matteo@gmail.com>
2019-01-03 12:40:56 +0100

    test

--- src/js/page/appartamenti.js
+++ src/js/page/appartamenti.js
@@ -1,12 +1,12 @@
 
 var info = collection('info');
-
+var appartamenti = collection('appartamenti');
 page
 ({ 
 	  path: 'appartamenti',
 	  layout: 'interna',
 	  blocks: { 
-	  			'content': 'appartamenti'
+	  			'content': ['appartamenti-intro', template('appartamenti', {appartamenti: appartamenti})]
 	          },
 	  title: info.title+' | Appartamenti',
 	  description: 'Il residence dispone di 8 appartamenti , di cui 4 monolocali e 4 bilocali, arredati con gusto e dotati di bagno, angolo cottura, forno a microonde, frigo, tv color, e stoviglie',
